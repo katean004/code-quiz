3bf6ff3624dde2f221a3251e2534261506c6ce2b
kate <kan004@ucr.edu>
2020-06-07 16:53:56 -0700

    hide buttons and quiz container

--- assets/quiz.js
+++ assets/quiz.js
@@ -5,3 +5,14 @@ var index = 0;
 let selectedAnswer;
 let timeInterval;
 
+$(document).ready(function(){
+    
+    $(".next-btn").hide(); // hide next button
+    $(".highscore-btn").hide(); // hide highscore button
+    $(".question-container").hide(); // hide question container
+    $(".timer").hide(); // hide timer
+    $(".green-alert").hide(); // hide green alert
+    $(".red-alert").hide(); // hide red alert
+
+
+})
