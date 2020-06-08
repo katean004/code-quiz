9434b05e928163aa192e41d4a0894e05a8cb1be2
kate <kan004@ucr.edu>
2020-06-07 17:18:06 -0700

    added render initials function

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -3,3 +3,22 @@ var initialsInput = document.querySelector("#initials-text");
 var initialsForm = document.querySelector("#initials-form");
 var initialsList = document.querySelector("#initials-list");
 var initialsScores = [];
+
+init();
+
+function renderInitials() {
+  // Clear initialsList element and update todoCountSpan
+  initialsList.innerHTML = "";
+
+  // Render a new li for each todo
+  for (var i = 0; i < initialsScores.length; i++) {
+    var initial = initialsScores[i];
+
+    var li = document.createElement("li");
+    li.textContent = initial;
+    li.setAttribute("data-index", i);
+
+    initialsList.appendChild(li);
+  }
+}
+
