49b9e05b4e80c1b615df15209ef1b4b2b64c4da9
kate <kan004@ucr.edu>
2020-06-07 17:06:37 -0700

    added questions js and 2 questions

---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+const questions = [
+    {
+        question: "Inside which HTML element do we put the Javascript?",
+        answers: [
+            { text: "<javascript>"},
+            { text: "<scripting>"},
+            { text: "<script>"},
+            { text: "<js>"}
+        ],
+        correct: "<script>"
+    },
+    {
+        question: "Where is the correct place to insert a Javascript?",
+        answers: [
+            { text: "<body> section"},
+            { text: "<head> section"},
+            { text: "both the <head> and the <body>"},
+            { text:"<title>"}
+        ],
+        correct : "both the <head> and the <body>"
+    },
+
+]
