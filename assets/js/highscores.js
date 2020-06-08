ba69f16c70f8860895dd1a9ad152bacf8bbd04e6
kate <kan004@ucr.edu>
2020-06-07 17:17:42 -0700

    added global variables for highscore

---
+++ assets/js/highscores.js
@@ -0,0 +1,5 @@
+// variables for highscore function
+var initialsInput = document.querySelector("#initials-text");
+var initialsForm = document.querySelector("#initials-form");
+var initialsList = document.querySelector("#initials-list");
+var initialsScores = [];
